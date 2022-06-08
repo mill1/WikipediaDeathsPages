6ac4527f2172ef3f630eba99885352f1da18c97c
mill1 <enijhuis@outlook.com>
2022-06-08 13:23:41 +0200

    Added test ResolveBritannicaReference

--- WikipediaDeathsPages.Tests/ReferenceResolverShould.cs
+++ WikipediaDeathsPages.Tests/ReferenceResolverShould.cs
@@ -21,11 +21,25 @@ namespace WikipediaDeathsPages.Tests
             referenceResolver = new ReferenceResolver(wikipediaReferencesMock.Object, logger);
         }
 
-        [Fact(DisplayName = "Resolve an Olympic reference")]
-        public void ResolveAnOlympicReference()
+        // Do not use Theory; Fact per makes identification of broken source easier.        
+        [Fact(DisplayName = "Resolve Britannica reference")]
+        public void ResolveBritannicaReference()
+        {
+            const string name = "Utpal Dutt";
+            var deathDate = new DateTime(1993, 8, 19);
+            var dateOfDeathRef = "enwiki~!Encyclopædia Britannica Online~!Encyclopædia Britannica Online ID: biography/Utpal-Dutt~!subject named as: Utpal Dutt";
+
+            var expectedSubstring = $"title={name} |url=https://www.britannica.com/biography/Utpal-Dutt |website=britannica.com";
+            var actualString = referenceResolver.Resolve(deathDate, dateOfDeathRef, name, null);
+
+            Assert.Contains(expectedSubstring, actualString);
+        }
+
+        [Fact(DisplayName = "Resolve Olympic reference")]
+        public void ResolveOlympicReference()
         {
             const string name = "Fanny Blankers-Koen";
-            var deathDate = new DateTime(2004, 1, 25);             
+            var deathDate = new DateTime(2004, 1, 25);      
             wikipediaReferencesMock.Setup(_ => _.GetIdsOfName(name)).Returns(new List<int> { 73711 });
 
             var expectedSubstring = $"title=Olympedia – {name} |url=http://www.olympedia.org/athletes/73711 |website=olympedia.org";

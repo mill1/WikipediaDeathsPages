59d3d52067dce8c65308fe7f31c4d622e3f842d3
mill1 <enijhuis@outlook.com>
2022-06-13 12:26:22 +0200

    wip: before ng refactor

--- WikipediaDeathsPages.Web/ClientApp/src/app/sandbox/sandbox.component.ts
+++ WikipediaDeathsPages.Web/ClientApp/src/app/sandbox/sandbox.component.ts
@@ -9,7 +9,7 @@ export class SandboxComponent {
 
   MAXWEBSITES = 5;
   validWebsites = this.MAXWEBSITES;    // makes sure that allSitesOk() is true at init (show #elseBlock2)
-  checkedWebsites = this.MAXWEBSITES;; // makes sure button is enabled  
+  checkedWebsites = this.MAXWEBSITES;  // makes sure button is enabled  
   checkResults: boolean[] = new Array(this.MAXWEBSITES).fill(false);
   checkStatus: string;
 
@@ -21,7 +21,7 @@ export class SandboxComponent {
     this.checkedWebsites = 0;
     this.checkStatus = 'Checking reference websites...';
     this.checkWebsite(0, 'https://www.britannica.com/biography/Utpal-Dutt', 'Utpal Dutt', 'August 19, 1993');
-    this.checkWebsite(1, 'http://www.britannica.com/EBchecked/topic/18816/Viktor-Amazaspovich-Ambartsumian', 'Viktor Ambartsumian', 'August 12, 1996');
+    this.checkWebsite(1, 'http://www.britannica.com/EBchecked/topic/18816/Viktor-Amazaspovich-Ambartsumian', 'Viktor Ambartsumian', 'August 12, 199996');
     this.checkWebsite(2, 'https://www.independent.co.uk/news/people/obituary-stanley-woods-1488284.html', 'Obituary: Stanley Woods', '<amp-state id="digitalData"');
     this.checkWebsite(3, 'https://www.independent.co.uk/incoming/obituary-harold-shepherdson-5649167.html', 'Obituary: Harold Shepherdson', '<amp-state id="digitalData"');
     this.checkWebsite(4, 'https://www.ibdb.com/broadway-cast-staff/88297', 'Tom Fuccello', 'Aug 16, 1993');
@@ -54,8 +54,6 @@ export class SandboxComponent {
     return this.validWebsites == this.MAXWEBSITES;
   }
 
-  //  -----
-
   getImageSource(found:boolean):string{
     if(found){
      return "../../assets/ok.png";
@@ -64,20 +62,5 @@ export class SandboxComponent {
      return "../../assets/nok.png";
     }
   }
-
-  /*
-  onFetch(){    
-    this.notAllSitesOk = true;    
-    let urlParam = encodeURIComponent('https://www.britannica.com/biography/Utpal-Dutt');
-
-    this.http.get<boolean>(this.baseUrl + 'reference/' + urlParam + '/' + 'Utpal Dutt' + '/' + 'August 19, 1993').subscribe(result => {         
-    this.validSiteBritannica = result;      
-    this.notAllSitesOk = false;          
-    }, error => {
-      this.notAllSitesOk = false;
-      console.error(error);}
-      );
-  }
-  */
 
 }
--- WikipediaDeathsPages.Web/ClientApp/src/app/websites-check/websites-check.component.ts
+++ WikipediaDeathsPages.Web/ClientApp/src/app/websites-check/websites-check.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, } from '@angular/core';
+import { HttpClient, } from '@angular/common/http';
 
 @Component({
   selector: 'app-websites-check',
   templateUrl: './websites-check.component.html',
   styleUrls: ['./websites-check.component.css']
 })
-export class WebsitesCheckComponent implements OnInit {
+export class WebsitesCheckComponent {
 
-  constructor() { }
+  urls: string[];
+  MAXWEBSITES: number;
+  validWebsites: number;
+  checkedWebsites: number;
+  checkResults: boolean[];
+  checkStatus: string;
 
-  ngOnInit() {
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {  
+
+    this.initializeWebsites();
+    this.MAXWEBSITES = this.urls.length;
+    this.validWebsites = this.MAXWEBSITES;    // makes sure that allSitesOk() is true at init (show #elseBlock2)
+    this.checkedWebsites = this.MAXWEBSITES;  // makes sure button is enabled  
+    this.checkResults = new Array(this.MAXWEBSITES).fill(false);
   }
 
+  initializeWebsites(){
+
+    this.urls=[
+      'https://www.britannica.com/biography/Utpal-Dutt',
+      'http://www.britannica.com/EBchecked/topic/18816/Viktor-Amazaspovich-Ambartsumian',
+      'https://www.independent.co.uk/news/people/obituary-stanley-woods-1488284.html',
+      'https://www.independent.co.uk/incoming/obituary-harold-shepherdson-5649167.html', 
+      'https://www.ibdb.com/broadway-cast-staff/88297' 
+    ];
+  }
+
+  checkWebsites(){
+    this.validWebsites = 0;
+    this.checkedWebsites = 0;
+    this.checkStatus = 'Checking reference websites...';
+    this.checkWebsite(0, this.urls[0], 'Utpal Dutt', 'August 19, 1993');
+    this.checkWebsite(1, this.urls[1], 'Viktor Ambartsumian', 'August 12, 1996');
+    this.checkWebsite(2, this.urls[2], 'Obituary: Stanley Woods', '<amp-state id="digitalData"');
+    this.checkWebsite(3, this.urls[3], 'Obituary: Harold Shepherdson', '<ap-state id="digitalData"');
+    this.checkWebsite(4, this.urls[4], 'Tom Fuccello', 'Aug 16, 1993');
+  }
+
+  checkWebsite(index: number, url:string, firstSearchPhrase: string, secondSearchPhrase: string){            
+    
+    this.http.get<boolean>(this.baseUrl + 'reference/' + encodeURIComponent(url) + '/' + firstSearchPhrase + '/' + secondSearchPhrase).subscribe(result => {         
+    this.checkResults[index] = result;      
+
+    if(this.checkResults[index])
+      this.validWebsites++;
+
+    this.finalizeCheck();
+    }, error => {
+      this.finalizeCheck();
+      console.error(error);
+    });        
+  }
+
+  finalizeCheck(){
+    this.checkedWebsites++;    
+    if (this.checkedWebsites == this.MAXWEBSITES){
+      this.checkStatus = 'Check complete';
+    }
+  }
+
+  allSitesOk(): boolean{
+    //console.log('allSitesOk: ' + (this.validWebsites == this.MAXWEBSITES) + ' (' + this.validWebsites + '-' + this.MAXWEBSITES + ')')
+    return this.validWebsites == this.MAXWEBSITES;
+  }
+
+  getImageSource(found:boolean):string{
+    if(found){
+     return "../../assets/ok.png";
+    }
+    else{
+     return "../../assets/nok.png";
+    }
+  }
 }
+
